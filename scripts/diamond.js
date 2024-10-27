2907521a35f6eb04e9e35df74ffc8faa3caa4be0
BreicheSinal <sinalbreiche@gmail.com>
2024-10-27 23:41:37 +0200

    Cleaned js file | Added input validation in py file)

--- scripts/diamond.js
+++ scripts/diamond.js
@@ -15,18 +15,15 @@ function changeColors()
     if (index >= colors.length) {
         index = 0;
     }
-    setTimeout(changeColors, 700);
+    setTimeout(changeColors, 450);
 };
 
 changeColors();
 
 function palindrome (){
     var inputRows = input.value;
-    console.log(inputRows);
 
     var output = [];
-
-    //pattern of *(1,3,5...)
 
     for(var x = 1; x <= inputRows; x++){
         for(var j = 1; j <= (2 * x -1) ; j++)
@@ -40,31 +37,5 @@ function palindrome (){
         output.push('\n');
     }
 
-    //console.log(output.join(''));
     outputSec.textContent = output.join('');
 };
-
-//C++ LOGIC
-/*
- for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= rows - i; j++) 
-            cout << " ";
-        
-        for (int j = 1; j <= (2 * i - 1); j++)
-            cout << "*";
-        
-        cout << endl;
-    }
-
-for (int x = inputRows - 1; x >= 1; x--) {
-    for (int y = 1; y <= inputRows - x; y++) {
-        cout << ' '; 
-    }
-
-    for (int j = 1; j <= (2 * x - 1); j++) {
-        cout << '*';
-    }
-
-    cout << endl; 
-}
-*/ 
--- scripts/diamond.py
+++ scripts/diamond.py
@@ -10,15 +10,18 @@ def palindrome(rows):
         for j in range(1, 2 * x):
             output.append('*')
         output.append('\n')
-        
+            
     for x in range(rows -1, 0, -1):
         for y in range(1, rows - x + 1):
             output.append(' ')
         for j in range(1, 2 * x):
             output.append('*')
         output.append('\n')
-        
     return output
 
-palindrome(num)
+if(num > 0 | num != None):
+    palindrome(num)
+else:
+    print('\n INVALID INPUT MATE ;)')
+
 print(''.join(output))
